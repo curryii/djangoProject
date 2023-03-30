3717677f78e99fdda48402e203f2f64bd82fbc0a
curry <1982586543@qq.com>
2023-03-30 23:06:25 +0800

    v0.9

--- projects/urls.py
+++ projects/urls.py
@@ -1,13 +1,24 @@
 from django.urls import path
 from projects import views
-from projects.views import ProjectView, ProjectViewId
+from projects.views import ProjectView, ProjectViewId, ProjectViewSet
 
 """
 定义类视图路由
 path('project/', ProjectView.as_view())
 """
 
+"""path('project/', ProjectView.as_view()),
+    path('project/<int:id>/', ProjectViewId.as_view()),"""
+
 urlpatterns = [
-    path('project/', ProjectView.as_view()),
-    path('project/<int:id>/', ProjectViewId.as_view())
+    path("project/", views.ProjectViewSet.as_view({
+        "get": "list",
+        "post": "create"
+    })),
+    path("project/<int:id>/", views.ProjectViewSet.as_view({
+        "get": "retrieve",
+        "put": "update",
+        "patch": "partial_update",
+        "delete": "destroy"
+    }))
 ]
--- projects/views.py
+++ projects/views.py
@@ -16,93 +16,129 @@ from rest_framework import filters
 from .serializers import ProjectSerializer, ProjectModelSerializer
 from utils.pagination import PageNumberPagination
 from rest_framework import mixins
+from rest_framework import generics
+from rest_framework import viewsets
 
-# Mixin拓展类
-"""class ListModelMixin:
-    def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.get_serializer(instance=page, many=True)
-            return self.get_paginated_response(serializer.data)
-        serializer = self.get_serializer(instance=queryset, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)"""
+"""
+class ListCreateAPIView(mixins.CreateModelMixin,
+                        mixins.ListModelMixin,
+                        GenericAPIView):
+    获取所以项目信息
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+    创建项目信息
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+"""
 
-"""class CreateModelMixin:
-    def create(self, request, *args, **kwargs):
-        req_ser = self.get_serializer(data=request.data)
-        req_ser.is_valid(raise_exception=True)
-        req_ser.save()
-        return Response(req_ser.data, status=status.HTTP_201_CREATED)"""
+"""
+a.直接继承Mixin拓展类，拓展类只提供了action方法
+b.action方法有哪些呢?
+    list --> 获取列表数据
+    retrieve --> 获取详情数据
+    create --> 创建数据
+    update --> 更新数据（完整)
+    partial_update --> 更新数据（部分)
+    destroy --> 删除数据
+c.类视图往往只能识别如下方法?
+    get --> list
+    get --> retrieve
+    post --> create
+    put --> update
+    patch --> partial_update
+    delete --> destroy
+d.为了进一步优化代码，需要使用具体的通用视图 XXXAPIView
+"""
 
-
-class ProjectView(
-    mixins.CreateModelMixin,
-    mixins.ListModelMixin,
-    GenericAPIView
-):
+"""
+    class ProjectView(generics.ListCreateAPIView):
     queryset = Projects.objects.all()
     serializer_class = ProjectModelSerializer
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
     search_fields = ["id", "^name", "=leader"]
     pagination_class = PageNumberPagination
-
     ordering_fields = ["id", "name", "leader"]
+"""
 
-    """获取所以项目信息"""
-
+"""
+    获取所以项目信息
     def get(self, request, *args, **kwargs):
-        """queryset = self.filter_queryset(self.get_queryset())
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.get_serializer(instance=page, many=True)
-            return self.get_paginated_response(serializer.data)
-        serializer = self.get_serializer(instance=queryset, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)"""
-        """
-        a. python中支持多重继承，一个类可以同时继承多个父类
-        b.类中的方法和属性是按照_mro_所指定的继承顺序进行搜索
-        """
         return self.list(request, *args, **kwargs)
-
-    """创建项目信息"""
-
+    创建项目信息
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
+"""
 
+"""
+mixins.RetrieveModelMixin,
+mixins.UpdateModelMixin,
+mixins.DestroyModelMixin,
+GenericAPIView
+"""
 
-class ProjectViewId(
-    mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.DestroyModelMixin,
-    GenericAPIView
-):
+"""
+    class ProjectViewId(generics.RetrieveUpdateDestroyAPIView):
     queryset = Projects.objects.all()
     serializer_class = ProjectModelSerializer
-
     lookup_url_kwarg = "id"
+"""
 
-    """获取所以单笔项目信息"""
-
+"""
+    获取所以单笔项目信息
     def get(self, request, *args, **kwargs):
-        """project_obj = self.get_object()
-        serializer = self.get_serializer(instance=project_obj)
-        return Response(serializer.data, status=status.HTTP_200_OK)"""
         return self.retrieve(request, *args, **kwargs)
-
-    """更新项目信息"""
-
+    更新项目信息
     def put(self, request, *args, **kwargs):
-        """project_d = self.get_object()
-        req_ser = self.get_serializer(instance=project_d, data=request.data)
-        req_ser.is_valid(raise_exception=True)
-        req_ser.save()
-        return Response(req_ser.data, status=status.HTTP_201_CREATED)"""
         return self.update(request, *args, **kwargs)
-
-    """删除项目信息"""
-
+    删除项目信息
     def delete(self, request, *args, **kwargs):
-        """self.get_object().delete()
-        return Response({"删除项目信息": "ok"}, status=status.HTTP_204_NO_CONTENT)"""
         return self.destroy(request, *args, **kwargs)
+"""
+
+"""
+a.可以继承视图集父类ViewSet
+b.在定义url路由条目时，支持给as_view传递字典参数（请求方法名与具体调用的action方法名的一一对应关系
+c. ViewSet继承了ViewSetMixin, views.APIView
+d.具备APIView的所有功能
+e.继事ViewSetMixin,所有具备持给as_view传递字典参数（请求方法名与具体调用的action方法名的一一对应关系
+"""
+
+"""
+class ProjectViewSet(
+mixins.ListModelMixin,
+mixins.RetrieveModelMixin,
+mixins.CreateModelMixin,
+mixins.UpdateModelMixin,
+mixins.DestroyModelMixin,
+viewsets.GenericViewSet):
+"""
+
+"""定义视图集"""
+
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Projects.objects.all()
+    serializer_class = ProjectModelSerializer
+    lookup_url_kwarg = "id"
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ["id", "^name", "=leader"]
+    pagination_class = PageNumberPagination
+    ordering_fields = ["id", "name", "leader"]
+
+    """def list(self, request, *args, **kwargs):
+        pass
+
+    def retrieve(self, request, *args, **kwargs):
+        pass
+
+    def create(self, request, *args, **kwargs):
+        pass
+
+    def update(self, request, *args, **kwargs):
+        pass
+
+    def partial_update(self, request, *args, **kwargs):
+        pass
+
+    def destroy(self, request, *args, **kwargs):
+        pass"""
